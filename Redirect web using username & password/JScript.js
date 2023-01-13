52690946f60bddba858ed479a8ee510d0bf56a6a
Ammar Karim <azlankarim660@gmail.com>
2023-01-14 01:54:20 +0500

    Add files via upload
    username: admin , password: admin

---
+++ Redirect web using username & password/JScript.js
@@ -0,0 +1,8 @@
+var username = prompt("username")
+var password = prompt("password")
+
+if (username == "admin" && password == "admin") {
+    window.open('https://a-beautiful-gift-73.netlify.app/');
+} else {
+    alert('Please enter correct username or password!');
+} 
